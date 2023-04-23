ecfca95eb08aa148bf300760dc0d34118bd953b7
Chris Duzan <cgduzan@gmail.com>
2023-04-23 16:31:13 -0400

    ♻️ One more refactor item

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,7 @@ import { startStandaloneServer } from '@apollo/server/standalone'
 import { Context, getContext } from './modules/context.js'
 import modules from './modules/index.js'
 
-const server = new ApolloServer<Context>({
-  typeDefs: modules.typeDefs,
-  resolvers: modules.resolvers,
-})
+const server = new ApolloServer<Context>(modules)
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
